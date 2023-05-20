c5e7ada6f62fd4af574500df47b86a66b434badc
Özgür Abi <ozgur.abi@ug.bilkent.edu.tr>
2023-05-20 16:41:59 +0300

    example response

--- server.js
+++ server.js
@@ -12,4 +12,11 @@ app.get("/api/admins", (req, res) => {
   res.json({ admins: ["Serhat", "Özgür"] });
 });
 
+app.get("/api/exres", (req, res) => {
+   fetch("https://api.spoonacular.com/recipes/{716429}/information")
+   .then(function(response){
+	   res.json(response);
+   });
+});
+
 app.listen(8080, () => console.log("Server started on port 5000"));
